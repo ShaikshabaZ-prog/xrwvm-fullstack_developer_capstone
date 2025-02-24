ca226ea99551d3e2e525018cd77e5f56086e4f0c
ShaikshabaZ-prog <shaikshabaz7075@gmail.com>
2025-02-24 20:19:24 +0530

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -14,11 +14,12 @@ sentiment_analyzer_url = os.getenv(
 # def get_request(endpoint, **kwargs):
 # Add code for get requests to back end
 
+
 def get_request(endpoint, **kwargs):
     params = ""
     if (kwargs):
         for key, value in kwargs.items():
-            params=params + key + "=" + value + "&"
+            params = params + key + "=" + value + "&"
 
     request_url = backend_url+endpoint+"?"+params
 
@@ -47,10 +48,11 @@ def analyze_review_sentiments(text):
 
 # def post_review(data_dict):
 
+
 def post_review(data_dict):
     request_url = backend_url+"/insert_review"
     try:
-        response = requests.post(request_url,json=data_dict)
+        response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
     except requests.exceptions.RequestException as err:
