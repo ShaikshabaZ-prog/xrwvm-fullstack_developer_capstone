6d4435bd015bc108503678ed10862c87e1558ae5
ShaikshabaZ-prog <shaikshabaz7075@gmail.com>
2025-02-24 23:09:16 +0530

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,10 +28,13 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'https://u222n1a0542-8000.theiadockernext
--0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS = ['https://u222n1a0542-8000.theiadockernext
--0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+ALLOWED_HOSTS = [
+    'localhost',
+    'https://u222n1a0542-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+]
+CSRF_TRUSTED_ORIGINS = [
+    'https://u222n1a0542-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
