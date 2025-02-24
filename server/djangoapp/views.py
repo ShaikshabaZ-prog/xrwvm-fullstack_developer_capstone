39e51766e88d899f57d07a1feb0821393657144b
ShaikshabaZ-prog <shaikshabaz7075@gmail.com>
2025-02-24 22:11:00 +0530

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -113,8 +113,7 @@ def get_dealer_reviews(request, dealer_id):
         reviews = get_request(endpoint)
         if reviews is not None:
             for review_detail in reviews:
-                response =
-                analyze_review_sentiments(review_detail['review'])
+                response = analyze_review_sentiments(review_detail['review'])
                 print(response)
                 if response is not None:
                     review_detail['sentiment'] = response['sentiment']
